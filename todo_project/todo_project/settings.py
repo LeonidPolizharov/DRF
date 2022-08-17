84578bdabdb8e742608c73866a803f0bd130832d
LeonidPolizharov <polizharov@mail.ru>
2022-08-17 23:13:13 +0300

    lesson-5

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -135,7 +135,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:37745",
     "http://localhost:3000",
+    "http://localhost:37745",
 ]
 
 
@@ -144,6 +147,6 @@ REST_FRAMEWORK = {
 #         'rest_framework.permissions.DjangoModelPermisionsOrAnonReadOnly'
 #     ]
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 100
 }
